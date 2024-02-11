da88c99a4fe38126310487d62ddae3d318e2025b
Fernando Robledo <fernando.robledoruiz@gmail.com>
2024-02-11 10:34:35 -0500

    Setting GUI

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title='Multiple PDF Chat',
+                       page_icon='🦄', layout='wide')
+    st.header('Chat with multiple PDFs 🦄')
+    st.text_input('Ask a question about your documents:')
+
+    with st.sidebar:
+        st.subheader('Your documents:')
+        st.file_uploader('Upload your PDFs here and click on Process')
+        st.button('Process')
+
+
+if __name__ == '__main__':
+    main()
