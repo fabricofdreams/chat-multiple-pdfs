752079c54cfbc2fca5ea251576b3b888fd804be4
Fernando Robledo <fernando.robledoruiz@gmail.com>
2024-02-11 13:30:27 -0500

    Splitting text into smaller chunks

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 
 def get_pdf_text(pdf_docs):
@@ -13,6 +14,16 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+
+def get_text_chunks(raw_text):
+    """
+    Split the text into small chunks
+    """
+    text_splitter = CharacterTextSplitter(
+        separator='\n', chunk_size=1000, chunk_overlap=200, length_function=len)
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
 
 
 def main():
@@ -33,6 +44,7 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
 
                 # Split content into small chunks
+                text_chunks = get_text_chunks(raw_text)
 
                 # Create embeddins for each chunk
 
