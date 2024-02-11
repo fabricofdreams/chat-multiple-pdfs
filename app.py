c5cc7c058d0313269b8b8f1a02e38699a2dbb9fa
Fernando Robledo <fernando.robledoruiz@gmail.com>
2024-02-11 12:00:58 -0500

    Getting the PDF content

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    """
+    Get the text from a PDF
+    """
+    text = ''
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
 def main():
@@ -12,8 +25,16 @@ def main():
 
     with st.sidebar:
         st.subheader('Your documents:')
-        st.file_uploader('Upload your PDFs here and click on Process')
-        st.button('Process')
+        pdf_docs = st.file_uploader(
+            'Upload your PDFs here and click on Process', accept_multiple_files=True)
+        if st.button('Process'):
+            with st.spinner('Processing...'):
+                # Get the PDFs content
+                raw_text = get_pdf_text(pdf_docs)
+
+                # Split content into small chunks
+
+                # Create embeddins for each chunk
 
 
 if __name__ == '__main__':
